833d117787e2c22ab26df3d17d5dd4a05a6cc03b
tflin <tflins@163.com>
2019-04-13 20:17:54 +0800

    vue-socket.io初体验

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const Koa = require('koa')
 const Router = require('koa-router')
+const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const mongoose = require('mongoose')
+const socket = require('socket.io')
 
 const port = process.env.PORT || 5000
 
-// 实例化 koa,Router 对象
+// 实例化 Koa, Router 对象
 const app = new Koa()
 const router = new Router()
+// 创建服务器
+const server = require('http').createServer(app.callback());
+// io
+const io = socket(server)
 
 // 配置中间件
 app.use(bodyParser())
@@ -19,6 +24,12 @@ router.get('/', async ctx => {
   ctx.body = {msg: 'hello'}
 })
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('一个用户已连接')
+  io.emit('login', '登录')
+})
+
+
+server.listen(port, () => {
   console.log(`监听${port}中`)
 })
